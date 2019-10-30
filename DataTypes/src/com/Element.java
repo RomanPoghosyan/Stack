522c715cdbe2070f928cc98d314ed0b7305faabf
RomanPoghosyan <roman.poghos@gmail.com>
2019-10-30 09:00:14 +0400

    Stack

---
+++ DataTypes/src/com/Element.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Element {
+    String s;
+    Element prev;
+    Element(String s, Element prev){
+        this.s = s;
+        this.prev = prev;
+    }
+}
---
+++ DataTypes/src/com/Test.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        Stack st = new Stack();
+        st.push("A");
+        st.push("B");
+        st.push("C");
+        st.push("D");
+//        System.out.println(st.pop());
+//        System.out.println(st.pop());
+//        System.out.println(st.pop());
+//        System.out.println(st.pop());
+//        System.out.println(st.pick());
+        System.out.println(st.size());
+    }
+}
